7de14002b749a6991d817f9368dda1497a6f70a5
sergey.andreev <Andreev.S.Ser@sberbank.ru>
2026-04-23 00:45:20 +0300

    Вынес заполнение тестовыми данными из main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,22 +10,11 @@ import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     //инициализация хранилища
-    val archives: MutableList<Archive> = mutableListOf()
-    //заполнение хранилища тестовыми данными
-    val notes: MutableList<Note> = mutableListOf()
-    notes.add(Note("Заметка 1", "На работе платят бабло"))
-    notes.add(Note("Заметка 2", "Но работать надо на ней"))
-    notes.add(Note("Заметка 3", "Я не против первого но"))
-    notes.add(Note("Заметка 4", "Без второго мне веселей"))
-
-    archives.add(Archive("Работа", notes))
-    archives.add(Archive("Дом"))
-    archives.add(Archive("Спорт"))
-    archives.add(Archive("Хобби"))
+    val archives: MutableList<Archive> = getTestData()
 
     //стартовый экран
     var currentScreen: Screen = ArchiveListScreen(archives)
-    var currentArchive: Int = 0
+    var currentArchiveIndex = 0
 
     //логика навигации и запуска экранов
     fun navigateTo(action: NavigationAction) {
@@ -33,9 +22,9 @@ fun main(args: Array<String>) {
             is NavigationAction.Back -> {
                 when(currentScreen) {
                     is NoteListScreen -> ArchiveListScreen(archives)
-                    is NoteViewScreen -> NoteListScreen(archives[currentArchive].notes)
+                    is NoteViewScreen -> NoteListScreen(archives[currentArchiveIndex].notes)
                     is ArchiveCreateScreen -> ArchiveListScreen(archives)
-                    is NoteCreateScreen -> NoteListScreen(archives[currentArchive].notes)
+                    is NoteCreateScreen -> NoteListScreen(archives[currentArchiveIndex].notes)
                     else -> ArchiveListScreen(archives)
                 }
             }
@@ -44,8 +33,8 @@ fun main(args: Array<String>) {
             }
             is NavigationAction.CreateArchive -> ArchiveCreateScreen()
             is NavigationAction.OpenArchive -> {
-                currentArchive = action.archiveIndex
-                NoteListScreen(archives[currentArchive].notes)
+                currentArchiveIndex = action.archiveIndex
+                NoteListScreen(archives[currentArchiveIndex].notes)
             }
             is NavigationAction.CreateNote -> NoteCreateScreen()
             is NavigationAction.OpenNote -> NoteViewScreen(action.note)
@@ -54,13 +43,22 @@ fun main(args: Array<String>) {
                 ArchiveListScreen(archives)
             }
             is NavigationAction.SaveNoteAndBack -> {
-                archives[currentArchive].notes.add(action.note)
-                NoteListScreen(archives[currentArchive].notes)
+                archives[currentArchiveIndex].notes.add(action.note)
+                NoteListScreen(archives[currentArchiveIndex].notes)
             }
         }
-
         currentScreen.start(::navigateTo)
     }
-
     currentScreen.start(::navigateTo)
+}
+
+private fun getTestData() : MutableList<Archive> {
+    val archives: MutableList<Archive> = mutableListOf()
+    val notes: MutableList<Note> = mutableListOf()
+    notes.add(Note("Заметка 1", "На работе платят бабло"))
+    notes.add(Note("Заметка 2", "Но работать надо на ней"))
+    notes.add(Note("Заметка 3", "Я не против первого но"))
+    notes.add(Note("Заметка 4", "Без второго мне веселей"))
+    archives.add(Archive("Работа", notes))
+    return archives
 }
